b0aebba2aa43aabfe1ee4b6b05a9a3cc67ecc0b2
cvilanova <cyvila@gmail.com>
2019-03-07 15:21:26 -0500

    Added UserController

---
+++ src/Controller/UserController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+/**
+ *
+ */
+public class UserController {
+    
+    public void logIn(String username, String password) {
+        
+    }
+    
+    public void register(String username, String password) {
+        
+    }
+}
